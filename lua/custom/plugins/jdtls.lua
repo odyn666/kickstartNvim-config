0b06043516ebc5be1dd8f45bc0ac400ce05a9fc8
odyn666 <odynek666@wp.pl>
2026-06-02 15:04:04 +0200

    lsp fix

--- lua/custom/plugins/jdtls.lua
+++ lua/custom/plugins/jdtls.lua
@@ -69,4 +69,3 @@ vim.keymap.set('v', '<leader>crv', "<Esc><Cmd>lua require('jdtls').extract_varia
 vim.keymap.set('n', '<leader>crc', "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = 'Extract Constant' })
 vim.keymap.set('v', '<leader>crc', "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", { desc = 'Extract Constant' })
 vim.keymap.set('v', '<leader>crm', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", { desc = 'Extract Method' })
-
